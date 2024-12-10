c38590a2a0dfeb71eb1e2ad8caac647fe8a2ab1c
Daniel J. Szelogowski <24834480+danielathome19@users.noreply.github.com>
2024-12-10 05:39:40 +0000

    Update

--- perceptron/train.py
+++ perceptron/train.py
@@ -4,23 +4,23 @@ class Perceptron:
     def __init__(self, input_size, lr=0.1, epochs=10):
         self.W = np.zeros(input_size)
         self.b = 0
-        self.lr = lr
+        self.alpha = lr
         self.epochs = epochs
 
-    def activation_fn(self, x):
+    def activate(self, x):
         return 1 if x >= 0 else 0
 
     def predict(self, x):
         z = self.W.T.dot(x) + self.b
-        return self.activation_fn(z)
+        return self.activate(z)
 
     def fit(self, X, d):
         for _ in range(self.epochs):
             for i in range(d.shape[0]):
-                y = self.activation_fn(self.W.T.dot(X[i]) + self.b)
+                y = self.activate(self.W.T.dot(X[i]) + self.b)
                 e = d[i] - y
-                self.W = self.W + self.lr * e * X[i]
-                self.b = self.b + self.lr * e
+                self.W = self.W + self.alpha * e * X[i]
+                self.b = self.b + self.alpha * e
 
 # Training data for AND gate
 X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
