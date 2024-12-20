ceec200b885c446a542c04d22bbd1c328e142173
Daniel J. Szelogowski <24834480+danielathome19@users.noreply.github.com>
2024-12-20 17:24:10 +0000

    Got all demonstrations working

--- tensorflow/gan/inference.rs
+++ tensorflow/gan/inference.rs
@@ -1,29 +1,31 @@
 // TODO: redo from scratch to generate an image from the generator model?
-
-use onnxruntime::{environment::Environment, tensor::OrtOwnedTensor, GraphOptimizationLevel, LoggingLevel, session::Session};
-use ndarray::array;
+use image::GrayImage;
+use rand::thread_rng;
+use rand_distr::{Distribution, Normal};
+use python_rust_ai::{create_onnx_session, predict_outputs};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Load the ONNX model
-    let environment = Environment::builder()
-        .with_name("gan_inference")
-        .with_log_level(LoggingLevel::Warning)
-        .build()?;
-    let session = environment
-        .new_session_builder()?
-        .with_optimization_level(GraphOptimizationLevel::Basic)?
-        .with_model_from_file("gan_generator_model.onnx")?;
+    let session = create_onnx_session("models/tf_gan_generator_model.onnx")?;
 
-    // Prepare input data for inference
-    let input_data = array![[0.0; 100]];
-    let input_tensor_values = vec![input_data.into_dyn()];
-
-    // Perform inference
-    let outputs: Vec<OrtOwnedTensor<f32, _>> = session.run(input_tensor_values)?;
-
-    // Print the inference result
+    // Generate a random noise vector
+    let latent_dim = 100;
+    let mut rng = thread_rng();
+    let normal = Normal::new(0.0, 1.0).unwrap();
+    let input_data: Vec<f32> = (0..latent_dim).map(|_| normal.sample(&mut rng) as f32).collect::<Vec<f32>>();
+    let outputs = predict_outputs(&session, input_data, &[1, latent_dim])?;
+    
     for output in outputs {
-        println!("{:?}", output);
+        let output_array = output.try_extract::<f32>()?;
+        let mut generated_image = output_array.view().to_owned().into_raw_vec();
+        generated_image.iter_mut().for_each(|x| *x = (*x + 1.0) / 2.0);
+
+        // Convert f32 values (0.0 to 1.0) to u8 values (0 to 255)
+        let image_data: Vec<u8> = generated_image.iter().map(|&x| (x * 255.0) as u8).collect();
+
+        // Save the generated image
+        let image = GrayImage::from_raw(28, 28, image_data).unwrap();
+        image.save("data/generated_image.png")?;
+        println!("Image generation complete!");
     }
 
     Ok(())
